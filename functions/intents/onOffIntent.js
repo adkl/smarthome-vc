0a2612d21ade7c67fd804b9d89df44bb368d8e1e
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2019-07-20 17:06:42 +0200

    work in progress on onOffIntent

--- functions/intents/onOffIntent.js
+++ functions/intents/onOffIntent.js
@@ -1,7 +1,7 @@
 function processOnOffIntent(dbRoot, conv, thing, smarthome_toggle) {
     switch (thing) {
         case "air conditioning":
-            processClimateToggle();
+            processClimateToggle(dbRoot, conv, smarthome_toggle);
             break;
         case "windows":
             processWindowsToggle();
@@ -10,8 +10,37 @@ function processOnOffIntent(dbRoot, conv, thing, smarthome_toggle) {
     conv.ask(`${thing} successfully toggled to ${smarthome_toggle}`)
 }
 
-function processClimateToggle() {
+function parseClimateToggleInput(climateToggle) {
+    const climateOnPhrases = ['turn on', 'switch on'];
+    const climateOffPhrases = ['turn off', 'switch off', 'shut down'];
 
+    if (climateOffPhrases.indexOf(climateToggle.toLowerCase()) !== -1) {
+        return true;
+    }
+    else if (climateOnPhrases.indexOf(climateToggle.toLowerCase()) !== -1) {
+        return false;
+    }
+    return undefined;
+}
+
+function resolveClimateStatus(climateStatus) {
+    return (climateStatus === true) ? "on": "turned off"
+}
+
+async function processClimateToggle(dbRoot, conv, climate_toggle) {
+    const snapshot = await dbRoot.child('users/user-id-1234/air-condition-status').once('value');
+    const currentClimateStatus = snapshot.val();
+    const requestedClimateStatus = parseClimateToggleInput(smarthome_toggle);
+
+    if (requestedClimateStatus === undefined) {
+        conv.ask(`I can't understand this. Please try to be more precise.`);
+    }
+    if (currentClimateStatus === requestedClimateStatus) {
+        conv.ask(`The air conditioner is already ${resolveClimateStatus(currentClimateStatus)}.`)
+    }
+    else {
+        // push to tasks queue TODO
+    }
 }
 
 function processWindowsToggle() {
